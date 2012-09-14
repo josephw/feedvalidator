c546d0878509f050ce100e1bd5f91e9aa1877089
Joseph Walton <joe@kafsemo.org>
2012-09-14 22:23:26 +1000

    Run 2to3 for 'print' and 'except'.

--- src/missingWebPages.py
+++ src/missingWebPages.py
@@ -46,7 +46,7 @@ for n, o in inspect.getmembers(feedvalidator.logging, inspect.isclass):
   if rcname in show:
     fn = os.path.join('docs', rcname, n + '.html')
     if not(isfile(os.path.join(BASE, fn))):
-      print fn
+      print(fn)
       areMissing=True
 
 if areMissing:
